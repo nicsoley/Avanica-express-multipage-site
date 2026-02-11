4420900c127cca626f49114211e8910cc49a5c75
Ashley Avanica <avanicaashley@gmail.com>
2026-02-11 23:15:29 +0800

    Update server, style, index and add posts/blog files

--- server.js
+++ server.js
@@ -1,26 +1,47 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the public folder
+// Serve static files
 app.use(express.static("public"));
 
-// Define Routes for diff pages 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// Home page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+// About page
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
 });
 
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+// Contact page
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "contact.html"));
 });
 
-// Start server
+// Blog page
+app.get("/blog", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "blog.html"));
+});
+
+// API route for blog posts
+app.get("/api/posts", (req, res) => {
+  const postsPath = path.join(__dirname, "data", "posts.json");
+  
+  // Using the asynchronous version is much better for performance
+  fs.readFile(postsPath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to read data" });
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// Start server (LAST)
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
